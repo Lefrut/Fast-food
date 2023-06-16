be702ad9c5a66d694fb596acdfb4a04d4956f0a8
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-06-16 14:07:05 +0300

    navigation to bottom screen - uses navigation component

---
+++ app/src/main/java/com/dashkevich/fast_food/screens/bottom/BottomFragment.kt
@@ -0,0 +1,16 @@
+package com.dashkevich.fast_food.screens.bottom
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dashkevich.fast_food.R
+
+class BottomFragment : Fragment(R.layout.fragment_bottom) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
